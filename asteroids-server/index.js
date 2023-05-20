b31673785fb83935bc104ca341e391a9758c6174
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-20 14:10:14 +0200

    feat(server): init server project

---
+++ asteroids-server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
+    }
+);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    }
+);
+
