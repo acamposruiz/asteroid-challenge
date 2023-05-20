17de4b0bda2aa36a7ce259fdeb627d7534dfd914
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-20 23:34:13 +0200

    feat(server): install eslint

--- asteroids-server/index.js
+++ asteroids-server/index.js
@@ -1,72 +1,70 @@
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
+const express = require('express')
+const http = require('http')
+const cors = require('cors')
 
-const app = express();
-app.use(cors());
+const app = express()
+app.use(cors())
 
-const favorites = [];
+const favorites = []
 
 app.get('/favorites', (request, response) => {
-    response.json(favorites);
-});
+  response.json(favorites)
+})
 
 app.post('/favorites/:id', (request, response) => {
-    const id = request.params.id;
-    const index = favorites.indexOf(id);
-    if (index === -1) {
-        favorites.push(id);
-    }
-    response.json(favorites);
-});
+  const id = request.params.id
+  const index = favorites.indexOf(id)
+  if (index === -1) {
+    favorites.push(id)
+  }
+  response.json(favorites)
+})
 
 app.delete('/favorites/:id', (request, response) => {
-    const id = request.params.id;
-    const index = favorites.indexOf(id);
-    if (index > -1) {
-        favorites.splice(index, 1);
-    }
-    response.json(favorites);
-});
+  const id = request.params.id
+  const index = favorites.indexOf(id)
+  if (index > -1) {
+    favorites.splice(index, 1)
+  }
+  response.json(favorites)
+})
 
 app.get('/neo/rest/v1/feed', (request, response) => {
-    const options = {
-        hostname: 'api.nasa.gov',
-        port: 80,
-        path: request.url,
-        method: request.method
-    };
-    response.setHeader('Access-Control-Allow-Origin', '*');
+  const options = {
+    hostname: 'api.nasa.gov',
+    port: 80,
+    path: request.url,
+    method: request.method
+  }
+  response.setHeader('Access-Control-Allow-Origin', '*')
 
-    const proxy = http.request(options, (res) => {
-        res.on('data', (chunk) => {
-            response.write(chunk);
-        });
+  const proxy = http.request(options, (res) => {
+    res.on('data', (chunk) => {
+      response.write(chunk)
+    })
 
-        res.on('end', () => {
-            response.end();
-        });
+    res.on('end', () => {
+      response.end()
+    })
 
-        
-        response.writeHead(res.statusCode, res.headers);
-    });
+    response.writeHead(res.statusCode, res.headers)
+  })
 
+  request.on('data', (chunk) => {
+    proxy.write(chunk)
+  })
 
-    request.on('data', (chunk) => {
-        proxy.write(chunk);
-    });
+  request.on('end', () => {
+    proxy.end()
+  })
 
-    request.on('end', () => {
-        proxy.end();
-    });
+  proxy.on('error', (err) => {
+    console.log('Error: ', err)
+  })
+})
 
-    proxy.on('error', (err) => {
-        console.log('Error: ', err);
-    });
-});
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
