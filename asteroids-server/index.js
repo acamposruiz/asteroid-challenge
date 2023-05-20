67a66274941454a8e00ccad6dce332720fd86076
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-20 15:32:02 +0200

    feat(server): implement forwarding to API

--- asteroids-server/index.js
+++ asteroids-server/index.js
@@ -1,10 +1,44 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World!');
-    }
-);
+const forwardRequestToAPI = (request, response) => {
+    const options = {
+        hostname: 'api.nasa.gov',
+        port: 80,
+        path: request.url,
+        method: request.method
+    };
+
+    const proxy = http.request(options, (res) => {
+        res.on('data', (chunk) => {
+            response.write(chunk, 'binary');
+        });
+
+        res.on('end', () => {
+            response.end();
+        });
+
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.writeHead(res.statusCode, res.headers);
+
+    });
+
+    request.on('data', (chunk) => {
+        proxy.write(chunk, 'binary');
+    });
+
+    request.on('end', () => {
+        proxy.end();
+    });
+
+    proxy.on('error', (err) => {
+        console.log('Error: ', err);
+    });
+
+};
+
+
+const app = http.createServer(forwardRequestToAPI);
+
 
 const PORT = process.env.PORT || 3001;
 
