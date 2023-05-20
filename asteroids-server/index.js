b8e225b5c2eed672980b68b4ea9a9030237b43a8
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-20 20:01:33 +0200

    feat(server): add favorites API

--- asteroids-server/index.js
+++ asteroids-server/index.js
@@ -3,29 +3,55 @@ const http = require('http');
 
 const app = express();
 
-app.all('*', (request, response) => {
+const favorites = [];
+
+app.get('/favorites', (request, response) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.json(favorites);
+});
+
+app.post('/favorites/:id', (request, response) => {
+    const id = request.params.id;
+    favorites.push(id);
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.json(favorites);
+});
+
+app.delete('/favorites/:id', (request, response) => {
+    const id = request.params.id;
+    const index = favorites.indexOf(id);
+    if (index > -1) {
+        favorites.splice(index, 1);
+    }
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.json(favorites);
+});
+
+app.get('/neo/rest/v1/feed', (request, response) => {
     const options = {
         hostname: 'api.nasa.gov',
         port: 80,
         path: request.url,
         method: request.method
     };
+    response.setHeader('Access-Control-Allow-Origin', '*');
 
     const proxy = http.request(options, (res) => {
         res.on('data', (chunk) => {
-            response.write(chunk, 'binary');
+            response.write(chunk);
         });
 
         res.on('end', () => {
             response.end();
         });
 
-        response.setHeader('Access-Control-Allow-Origin', '*');
+        
         response.writeHead(res.statusCode, res.headers);
     });
 
+
     request.on('data', (chunk) => {
-        proxy.write(chunk, 'binary');
+        proxy.write(chunk);
     });
 
     request.on('end', () => {
@@ -35,6 +61,7 @@ app.all('*', (request, response) => {
     proxy.on('error', (err) => {
         console.log('Error: ', err);
     });
+    
 });
 
 const PORT = process.env.PORT || 3001;
@@ -42,3 +69,5 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
