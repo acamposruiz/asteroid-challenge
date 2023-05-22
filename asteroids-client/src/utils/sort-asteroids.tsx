b1ba75bec94ca66d0528423f8f73dee3f067aeec
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-22 12:01:52 +0200

    feat(client): adapt sort logic to type data

--- asteroids-client/src/utils/sort-asteroids.tsx
+++ asteroids-client/src/utils/sort-asteroids.tsx
@@ -12,11 +12,16 @@ export const sortAsteroids = (sort: [keyof AsteroidModel, boolean] | null) => (a
   const valueA = a.hasOwnProperty(sortKey) ? a[sortKey] ?? 0 : 0
   const valueB = b.hasOwnProperty(sortKey) ? b[sortKey] ?? 0 : 0
 
-  if (valueA < valueB) {
-    return sortAsc ? -1 : 1
+  if (typeof valueA === 'number' && typeof valueB === 'number') {
+    return sortAsc ? valueA - valueB : valueB - valueA
   }
-  if (valueA > valueB) {
-    return sortAsc ? 1 : -1
+
+  if (typeof valueA === 'string' && typeof valueB === 'string') {
+    return sortAsc ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA)
+  }
+
+  if (typeof valueA === 'boolean' && typeof valueB === 'boolean') {
+    return sortAsc ? (valueA ? 1 : 0) - (valueB ? 1 : 0) : (valueB ? 1 : 0) - (valueA ? 1 : 0)
   }
 
   return 0
