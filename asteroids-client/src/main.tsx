db728fbce59c9ce132e5b1b4ba7e60f8c2144290
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-23 11:11:32 +0200

    refactor(client): get back strick mode from React

--- asteroids-client/src/main.tsx
+++ asteroids-client/src/main.tsx
@@ -1,10 +1,13 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { WrapperProvider } from './providers/wrapper-provider.tsx'
 import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <WrapperProvider>
-    <App />
-  </WrapperProvider>
+  <React.StrictMode>
+    <WrapperProvider>
+      <App />
+    </WrapperProvider>
+  </React.StrictMode>
 )
