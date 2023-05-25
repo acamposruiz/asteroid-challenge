8fb7c092f61ecec821bb382e443defcf9c375a5c
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-25 22:32:42 +0200

    feat(client): use of Suspense component from React

--- asteroids-client/src/core/app-router.tsx
+++ asteroids-client/src/core/app-router.tsx
@@ -1,23 +1,27 @@
+import { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+import { Loading } from '@/components'
 import { DetailPage, ErrorPage, HomePage, NotFoundPage } from '@/pages'
 
 import { CLIENT_PATHS } from './'
 
 export const AppRouter = () => {
   return (
-    <Router>
-      <Routes>
-        <Route index path={CLIENT_PATHS.HOME} element={<HomePage />} />
-        <Route
-          path={CLIENT_PATHS.ASTEROID_DETAIL}
-          element={<DetailPage />}
-        />
-        {['*', CLIENT_PATHS.NOT_FOUND].map((path, index) =>
-          <Route path={path} element={<NotFoundPage />} key={index} />
-        )}
-        <Route index path={CLIENT_PATHS.ERROR} element={<ErrorPage />} />
-      </Routes>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <Routes>
+          <Route index path={CLIENT_PATHS.HOME} element={<HomePage />} />
+          <Route
+            path={CLIENT_PATHS.ASTEROID_DETAIL}
+            element={<DetailPage />}
+          />
+          {['*', CLIENT_PATHS.NOT_FOUND].map((path, index) =>
+            <Route path={path} element={<NotFoundPage />} key={index} />
+          )}
+          <Route index path={CLIENT_PATHS.ERROR} element={<ErrorPage />} />
+        </Routes>
+      </Router>
+    </Suspense>
   )
 }
