2f7c508a82ae467ed3389a1f10e5a9e1c4819c54
Antonio2 Ignacio Campos Ruiz <antonio2ignaciocamposruiz@AT-RW0YQ279F6.local>
2023-05-25 16:38:20 +0200

    feat(server): setting enviroment vars

--- asteroids-server/index.js
+++ asteroids-server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const http = require('http')
 const cors = require('cors')
@@ -100,7 +101,7 @@ app.get('/asteroids-api/:type/:id?', (request, response) => {
   })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
